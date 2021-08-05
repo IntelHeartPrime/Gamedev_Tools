c9d2ad789a6f057a9cb3766b7a2ba1b042bfd434
燕欣 <1446598178@qq.com>
2021-08-05 08:25:16 +0800

    Update tools {props_config tool}

--- props_config_tool/Props_conifg_csv2Json.py
+++ props_config_tool/Props_conifg_csv2Json.py
@@ -1,128 +1,49 @@
 
-'''
-
-要输出的json格式
-{
-        "course_id": 17300,
-        "id": 17300,
-        "props": [
-            {
-                "balls": [
-                    1
-                ],
-                "clubs": [
-                    {
-                        "id": 31,
-                        "level": 11
-                    },
-                    {
-                        "id": 32,
-                        "level": 11
-                    }
-                ]
-            },
-            {
-                "balls": [
-                    1
-                ],
-                "clubs": [
-                    {
-                        "id": 37,
-                        "level": 6
-                    },
-                    {
-                        "id": 38,
-                        "level": 5
-                    }
-                ]
-            },
-            {
-                "balls": [
-                    1
-                ],
-                "clubs": [
-                    {
-                        "id": 37,
-                        "level": 6
-                    },
-                    {
-                        "id": 38,
-                        "level": 6
-                    }
-                ]
-            },
-            {
-                "balls": [
-                    1
-                ],
-                "clubs": [
-                    {
-                        "id": 37,
-                        "level": 5
-                    },
-                    {
-                        "id": 38,
-                        "level": 5
-                    }
-                ]
-            },
-            {
-                "balls": [
-                    1
-                ],
-                "clubs": [
-                    {
-                        "id": 31,
-                        "level": 11
-                    },
-                    {
-                        "id": 63,
-                        "level": 5
-                    }
-                ]
-            }
-        ],
-        "tour": 17
-    },
-'''
-
-# props config 工具的写法：
-# 母字典: courses_id:int ,id:int ,props(subdictionary):（balls: int or int_list , clubs(a list of dictionary): {id: int level: int}
-# 逐行读取xlsx数据并且导出到json
-
-
 from openpyxl import load_workbook
 import json
 
 wb = load_workbook("Props_config .xlsx")
 ws = wb.active
 
+json_dir = "Props_config.json"
+
 # test the data frame
 
-unit_dic = {}
-unit_dic.update({"course_id": 10600})
-unit_dic.update({"id": 10600})
-props_list = []
-props_dic = {"props": props_list}
+dicts_list = []
+row_index = 3
 
-unit_dic.update(props_dic)
-unit_dic.update({"tour": 17})
+while ws.cell(row_index,1).value != None:
+    unit_dic = {}
+    unit_dic.update({"course_id": ws.cell(row_index,1).value})
+    unit_dic.update({"id": ws.cell(row_index,2).value})
+    props_list = []
+    props_dic = {"props": props_list}
 
-props_list_unit = {}
-props_list_unit.update({"balls": [1, 2]})
-clubs_list = []
-props_list_unit.update({"clubs": clubs_list})
-clubs_list_unit1 = {"id": 26, "level": 4}
-clubs_list_unit2 = {"id": 27, "level": 5}
-clubs_list.append(clubs_list_unit1)
-clubs_list.append(clubs_list_unit2)
+    unit_dic.update(props_dic)
+    unit_dic.update({"tour": 17})
 
-props_list.append(props_list_unit)
+    for x in range(5):
+        if(ws.cell(row_index,5*x+4).value!= None) and (ws.cell(row_index,5*x+4).value != ""):
+            props_list_unit = {}
+            if (ws.cell(row_index,5*x+3).value!= None) and (ws.cell(row_index,5*x+3).value!=""):
+                props_list_unit.update({"balls": ws.cell(row_index,5*x+3).value})
+            else:
+                list_empty=[]
+                props_list_unit.update({"balls": list_empty})
+            clubs_list = []
+            props_list_unit.update({"clubs": clubs_list})
+            clubs_list_unit1 = {"id": ws.cell(row_index,5*x+4).value, "level": ws.cell(row_index,5*x+5).value}
+            clubs_list_unit2 = {"id": ws.cell(row_index,5*x+6).value, "level": ws.cell(row_index,5*x+7).value}
+            clubs_list.append(clubs_list_unit1)
+            clubs_list.append(clubs_list_unit2)
+            props_list.append(props_list_unit)
+    dicts_list.append(unit_dic)
+    print("complete " + str(row_index) + " line")
+    row_index = row_index + 1
 
-# test output the json file
 
-with open("Props_config.json", "w") as json_file:
-    json_str = json.dumps(unit_dic, indent=4)
+with open(json_dir, "w") as json_file:
+    json_str = json.dumps(dicts_list, indent=4)
     json_file.write(json_str)
 
 
