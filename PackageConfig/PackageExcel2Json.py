80190568e7c4bcdb2ae8bcebdb86c3ea7e448e7a
燕欣 <1446598178@qq.com>
2021-12-14 22:37:10 +0800

    Update tools

--- PackageConfig/PackageExcel2Json.py
+++ PackageConfig/PackageExcel2Json.py
@@ -12,25 +12,40 @@ def clean_null(input_value):
 
 
 
-wb = load_workbook("e2j.xlsx")
+wb = load_workbook("PropPackConf.xlsx")
 ws = wb.active
 
-json_dir = "Excel_to_Json.json"
+json_dir = "PropPackJson.json"
 
-unit_dic = {}
-unit_dic.update({"id": clean_null(ws.cell(2,2).value)})
-unit_dic.update({"show_time_start": clean_null(ws.cell(3,2).value)})
-unit_dic.update({"start_time": clean_null(ws.cell(4,2).value)})
-unit_dic.update({"show_time_end": clean_null(ws.cell(5,2).value)})
-unit_dic.update({"end_time": clean_null(ws.cell(6,2).value)})
-unit_dic.update({"join_before_end_time": clean_null(ws.cell(7,2).value)})
-min_level = {}
-unit_dic.update({"unlock_conditions": min_level})
-min_level.update({"min_level": clean_null(ws.cell(8,4).value)})
+dic_parent = {}
+row_index = 4
+while ws.cell(row_index, 1).value != None:
+    unit_dic = {}
+    dic_parent.update({str(ws.cell(row_index,1).value): unit_dic})
+    unit_dic.update({"id": ws.cell(row_index, 1).value})
+    unit_dic.update({"type": ws.cell(row_index, 2).value})
+
+    prop_list = []
+    unit_dic.update({"prop_list": prop_list})
+    for index in range(5):
+        if ws.cell(row_index, 5+5*index).value !=None:
+            dic_prop = {}
+
+            dic_prop.update({"prop_id": ws.cell(row_index,5+5*index).value})
+            dic_prop.update({"prop_num": ws.cell(row_index,6+5*index).value})
+            dic_prop.update({"prop_type": ws.cell(row_index,7+5*index).value})
+            dic_prop.update({"prop_color": ws.cell(row_index,8+5*index).value})
+
+        prop_list.append(dic_prop)
 
 
+
+
+    row_index = row_index + 1
+
+    print("Complete Line : " + str(row_index))
 
 with open(json_dir, "w") as json_file:
-    json_str = json.dumps(unit_dic, indent=4)
+    json_str = json.dumps(dic_parent, indent=4)
     json_file.write(json_str)
 
