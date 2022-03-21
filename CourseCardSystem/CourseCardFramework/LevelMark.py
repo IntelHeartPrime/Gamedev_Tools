e7a929dc259c4b77388dfaca1681095dac0901f2
yanxin <intelheartprime@outlook.com>
2022-03-21 21:52:20 +0800

    Update tools

--- CourseCardSystem/CourseCardFramework/LevelMark.py
+++ CourseCardSystem/CourseCardFramework/LevelMark.py
@@ -28,16 +28,53 @@ dic_cardDepository.update({"紫4": 18})
 dic_cardDepository.update({"传奇1": 19})
 dic_cardDepository.update({"传奇2": 20})
 
+dic_color = {}
+dic_color.update({"橙1": (255,165,79)})
+dic_color.update({"橙2": (255,165,79)})
+dic_color.update({"紫1": (132,112,255)})
+dic_color.update({"紫2": (132,112,255)})
+dic_color.update({"紫3": (132,112,255)})
+dic_color.update({"紫4": (132,112,255)})
+dic_color.update({"传奇1": (255,130,171)})
+dic_color.update({"传奇2": (255,130,171)})
+
+# 关卡颜色清零
+
+origin_color = ws1.range((1,1)).color
+
 row_start = 5
 while ws1.range((row_start, 3)).value != None:
-    chapter_id = ws1.range((row_start, 1)).value
-    level_id = ws1.range((row_start, 2)).value
+    ws1.range((row_start, 3)).color = origin_color
+    row_start = row_start + 1
+
+# 球场卡库颜色清零
+for col in range(13, 20):
+    for row in range(31, 41):
+        ws1.range((row, col)).color  = origin_color
+
+row_start = 5
+while ws1.range((row_start, 3)).value != None:
+    chapter_id = int(ws1.range((row_start, 1)).value)
+    level_id = int(ws1.range((row_start, 2)).value)
     card_string = ws1.range((row_start, 3)).value
-    card_lv = ws1.range((row_start, 4)).value
+    card_lv = int(ws1.range((row_start, 4)).value)
     print(" chapter_id = " + str(chapter_id) + " level = " + str(level_id) + " 场景 = " + str(card_string) + \
           " 场景等级 = " + str(card_lv))
 
+    # 遍历字典，获取目标颜色
+    for key_str in dic_color.keys():
+        if card_string == key_str:
+            ws1.range((row_start, 3)).color = dic_color[key_str]
+            break
+
+    # 标记球场卡库
+    col_index = dic_cardDepository[card_string]
+    row_index_cardDepository = 30 + card_lv
+    ws1.range((row_index_cardDepository, col_index)).color = dic_color[key_str]
+
+    
     row_start = row_start + 1
+
 
 
 
